fffb6ff44344e40ead517e41b058c86b078f2f83
NavpreetBrar <navpreetkaur@krishnais.com>
2016-03-02 14:27:49 +0530

    Fix notification (first name last name issue)

--- ParseStarterProject/AllNotificationList.swift
+++ ParseStarterProject/AllNotificationList.swift
@@ -131,23 +131,54 @@ class AllNotificationList: UITableViewController {
             let userNameStr = raw["username"] as? String ?? ""
             
             var notifyStrArr = notifyStr.componentsSeparatedByString(" ")
-            var userName : String = notifyStrArr [0]
-            
-            cell.userNameLabel?.text = "@" + userNameStr
-            cell.notificationTxtLabel?.text = notifyStr
-            cell.timeLabel?.text = timeStr
+            let userName : String = notifyStrArr [0]
+            cell.fullNameLabel?.text = ""
 
-//            cell.notificationTxtLabel?.text.attributedText = attributedText(userName)
+            var fullname : NSString = ""
+            if let firstName = raw["first_name"] as? NSString
+            {
+                fullname = firstName
+                if let lastName = raw["last_name"] as? NSString
+                {
+                    fullname = "\(firstName) \(lastName)"
+                }
+            }
+           
+          
+           
+            cell.timeLabel?.text = timeStr
             
-            var attributedString = NSMutableAttributedString(string: notifyStr as String, attributes: [NSFontAttributeName:UIFont.systemFontOfSize(13.0)])
+            let attributedString = NSMutableAttributedString(string: notifyStr as String, attributes: [NSFontAttributeName:UIFont.systemFontOfSize(11.0)])
             
-            let boldFontAttribute = [NSFontAttributeName: UIFont.boldSystemFontOfSize(13.0)]
+            let boldFontAttribute = [NSFontAttributeName: UIFont.boldSystemFontOfSize(11.0)]
             
             
             // Part of string to be bold
             attributedString.addAttributes(boldFontAttribute, range:  NSRange(location: 0,length: userName.characters.count))
-            
-            cell.notificationTxtLabel?.attributedText = attributedString
+//            cell.fullNameLabel.text = "FirstName"
+
+            if (fullname == "")
+            {
+                cell.fullNameLabel.font = UIFont.boldSystemFontOfSize(10.0)
+                cell.userNameLabel.font = UIFont.systemFontOfSize(11.0)
+                cell.userNameLabel.textColor = UIColor.blackColor()
+                cell.fullNameLabel.textColor = UIColor.grayColor()
+
+                cell.fullNameLabel?.text = "@" + userNameStr
+                cell.userNameLabel?.attributedText = attributedString
+                cell.notificationTxtLabel?.text = ""
+            }
+            else
+            {
+                cell.fullNameLabel.font = UIFont.boldSystemFontOfSize(11.0)
+                cell.userNameLabel.font = UIFont.systemFontOfSize(10.0)
+                cell.userNameLabel.textColor = UIColor.grayColor()
+                cell.fullNameLabel.textColor = UIColor.blackColor()
+                
+                cell.fullNameLabel?.text = fullname as String
+                cell.userNameLabel?.text = "@" + userNameStr
+                cell.notificationTxtLabel?.attributedText = attributedString
+            }
 
             
             let placeHolderimg = UIImage(named: "default-user-profile")
@@ -174,44 +205,7 @@ class AllNotificationList: UITableViewController {
         return cell
     }
     
-    
-//    override func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-//        let isEqual = (page_id == "<null>")
-//        if isEqual || page_id.isEmpty
-//        {
-//            return 0.0
-//        }
-//        else
-//        {
-//            return 40.0
-//        }
-//    }
-//
-//    override func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-//        let footerView = UIView(frame: CGRectMake(0, 0, tableView.frame.size.width, 40))
-//        footerView.backgroundColor = UIColor.blackColor()
-//        
-//        
-//        
-//        let btn: UIButton = UIButton(frame: CGRectMake(0, 0, tableView.frame.size.width, 40))
-//        btn.backgroundColor = UIColor.clearColor()
-//        btn.setTitle("Load More", forState: UIControlState.Normal)
-//        btn.addTarget(self, action: "loadMoreTapped:", forControlEvents: UIControlEvents.TouchUpInside)
-//        btn.tag = 1               // change tag property
-//        footerView.addSubview(btn) // add to view as subview
-//        
-//        return footerView
-//    }
-//    
-//    func loadMoreTapped(sender: UIButton!) {
-//        let btnsendtag: UIButton = sender
-//        if btnsendtag.tag == 1 {
-//            let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-//            appDelegate.hideActivityIndicator()
-//            allNotificationsAPI()
-//        }
-//    }
-
+  
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         let notificationPayload = self.notificationListArray[indexPath.row] as! [String : AnyObject]
         let payload = self.payloadArray[indexPath.row] as! [String : AnyObject]
@@ -280,34 +274,6 @@ class AllNotificationList: UITableViewController {
         }
     }
     
-    func process(payload payload: [String: AnyObject]) -> DeepLink? {
-        let link = DeepLink.from(payload: payload)
-        
-        // increase counter
-        if let link = link {
-            switch link {
-            case .MomentLink(_, let uuid, _):
-                DeepLink.timeline(uuid: uuid, completion: { (tl) -> Void in
-                    tl.hasNews = true
-                    tl.parent?.hasNews = true
-                    self.finish(payload: payload)
-                })
-            case .TimelineLink(_, let uuid):
-                DeepLink.timeline(uuid: uuid, completion: { (tl) -> Void in
-                    tl.hasNews = true
-                    tl.parent?.hasNews = true
-                    self.finish(payload: payload)
-                })
-            case .UserLink(_, _, let uuid):
-                DeepLink.user(uuid: uuid, completion: { (u) -> Void in
-                    u.hasNews = true
-                    self.finish(payload: payload)
-                })
-            }
-        }
-        return link
-    }
-   
     func processAsync(payload payload: [String: AnyObject], completion: (DeepLink?) -> Void) {
         let link = DeepLink.from(payload: payload)
         
@@ -367,7 +333,7 @@ class AllNotificationList: UITableViewController {
     }
 
     func processAsyncForUser(payload payload: [String: AnyObject], completion: (DeepLink?) -> Void) {
-        let link = DeepLink.fromNotification(payload: payload , likeOrCreateTimeline:                 timelineLikeOrComment )
+        let link = DeepLink.fromNotification(payload: payload , likeOrCreateTimeline: timelineLikeOrComment )
         
         // increase counter
         if let link = link {
